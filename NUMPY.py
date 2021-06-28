8d86313d8f803bc9b1225d04127cc1ea658b38e1
NihalPelvanoglu <82122322+NihalPelvanoglu@users.noreply.github.com>
2021-06-28 19:38:20 +0300

    Add files via upload

---
+++ NUMPY.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 23:31:31 2020
+
+@author: nihal
+"""
+
+# NUMPY 
+import numpy as np
+
+print(np.mean(Dataname)) # ortalama alır.
+
+print(round(0.246, 2)) # virgülden sonraki iki basamağı yuvarlar
+
+print(np.floor(Dataname)) # bir sonraki küçük elamana yuvarlar.
+
+print(np.ceil(Dataname)) # bir sonraki büyük elemana yuvarlar.
+
+print(sorted(Dataname)) # küçükten büyüğe sıralar.
+
+print(sorted(Dataname, reverse = True)) # büyükten küçüğe sıralar.
+
+print(text.lower()) # bütün harfleri küçük yapar.
+
+print(text.upper()) # bütün harfleri büyük yapar. 
+
+print(text.capitalize()) # sadece baş harfi büyük yapar.
+
+print(text.title()) # her kelimenin baş harfini büyük yapar.
+
+print(text.replace("SO", "very")) # kelimenin yerine yeni kelime atar. 
+                                  #!! ancak listelerde replace komutu çalışmaz.
+                                  
+print(text.index("o")) # istenilen elemanın bulunduğu yeri verir.
+
+                                 
+                                  
+                                  
